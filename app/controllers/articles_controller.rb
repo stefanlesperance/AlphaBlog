b8458fc26c86d94cc4ee67c7021e0ef3e2138138
Stefan Lesperance <slesperance@conamex.com>
2021-04-08 06:22:41 -0400

    Update Function
    Manual update function with accompanying notes and questions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,10 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def create
 		#by making it an ainstance variable, we can use it outside.
 		@article = Article.new(params.require(:article).permit(:title, :description))
@@ -28,6 +32,23 @@ class ArticlesController < ApplicationController
 		#This redirects to the specific artile. Rails is smart enough to extract the ID
 		#renders info to the screen render plain: params[:article]
 		#of particular note, the render will show you what is being passed via the params hash
+	end
+
+	def update
+		#The below bit takes the parameter provided by the Hash, the Article ID, and finds it.
+		@article = Article.find(params[:id])
+		#Once found, if the button is clicked, it whitelists the Title and Description, and tells them to compare and if need be change it.
+		if @article.update(params.require(:article).permit(:title, :description))
+			#Following that, if it is SUCCESSFUL (true/false) it will send a notice and redirect.
+			flash[:notice] = "Article was updated successfully"
+			#Wonder why redirect_to as opposed to render, but it might be Rails being smart enough to geuss, whereas render is needed for edit.
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/about'
   #I believe the below indicates which 'routes' are to be accepted/taken from resources
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
