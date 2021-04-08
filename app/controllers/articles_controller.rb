0ac1cc0912dcae01516d3019bf67b6001a41a7cb
Stefan Lesperance <slesperance@conamex.com>
2021-04-08 07:48:50 -0400

    Refactoring
    How to properly refactor code within Ruby on Rails. Use of Private in Controllers.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,10 @@
 class ArticlesController < ApplicationController
+	#This will summon the set_article method before each action i.e. show, edit etc. that requires an article to be set
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
 
 	def show
 		#retrieving data from the database using the Article ID - Correct Method Below
-		@article = Article.find(params[:id])
+
 		#incorrect method below
 		#article = Article.find(params[:id]) - Params is a hash, and will nab it from the HTML(?)
 	end
@@ -17,12 +19,11 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
 	end
 
 	def create
 		#by making it an ainstance variable, we can use it outside.
-		@article = Article.new(params.require(:article).permit(:title, :description))
+		@article = Article.new(set_params)
 		if @article.save
 			flash[:notice] = "Article was successfully created."
 			redirect_to @article
@@ -36,9 +37,8 @@ class ArticlesController < ApplicationController
 
 	def update
 		#The below bit takes the parameter provided by the Hash, the Article ID, and finds it.
-		@article = Article.find(params[:id])
 		#Once found, if the button is clicked, it whitelists the Title and Description, and tells them to compare and if need be change it.
-		if @article.update(params.require(:article).permit(:title, :description))
+		if @article.update(set_params)
 			#Following that, if it is SUCCESSFUL (true/false) it will send a notice and redirect.
 			flash[:notice] = "Article was updated successfully"
 			#Wonder why redirect_to as opposed to render, but it might be Rails being smart enough to geuss, whereas render is needed for edit.
@@ -50,9 +50,22 @@ class ArticlesController < ApplicationController
 
 	def destroy
 		#receiving article id is done via link, because Destroy does not have a view like the others.
-		@article = Article.find(params[:id])
 		@article.destroy
 		redirect_to articles_path
 	end
+
+	#Method to replace multiple calls
+
+	private
+
+	#method is locked to within the controller, nothing outside can access it.
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
+	def set_params
+		params.require(:article).permit(:title, :description)
+	end
+
 
 end
