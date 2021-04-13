20dde2f8232cae9011025d5dae12a50afebf79f3
Stefan Lesperance <slesperance@conamex.com>
2021-04-13 07:03:02 -0400

    Fixed Footer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#Adjusts the div class of field with errors that Rails adds to an input field with a validation error.
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
