2be1f79391e103b9764b8c671fff173d49ea8b08
Stefan Lesperance <slesperance@conamex.com>
2021-04-13 10:19:47 -0400

    Validation User Model

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-	
+	validates :username, uniqueness: true, presence: true, length: {minimum: 3, maximum: 25}
+	validates :email, uniqueness: true, presence: true, length: {maximum: 105}, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
 end
