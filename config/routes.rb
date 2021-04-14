02587c0e8682a5e064d455b0d017554b46958586
Stefan L'Esperance <slesperance@conamex.com>
2021-04-14 11:30:59 -0400

    Errors in my Git Comitting, re-adding material.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'pages/about'
   #I believe the below indicates which 'routes' are to be accepted/taken from resources
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :user, except: [:new]
 end
