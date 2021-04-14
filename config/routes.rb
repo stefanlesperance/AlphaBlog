99ec16ac9665f3554be0e9bfe34ceab5f0430c69
Stefan L'Esperance <slesperance@conamex.com>
2021-04-14 11:03:09 -0400

    Signup Form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/about'
-  #I believe the below indicates which 'routes' are to be accepted/taken from resources
   resources :articles
-end
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  #I believe the below indicates which 'routes' are to be accepted/taken from resources
+  end
