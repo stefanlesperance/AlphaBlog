2e10ff44292f8e2597c936c799ab138eb49c91a1
Stefan L'Esperance <slesperance@conamex.com>
2021-04-14 06:23:55 -0400

    Adding User Validations Finalized.

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
-	validates :username, uniqueness: true, presence: true, length: {minimum: 3, maximum: 25}
-	validates :email, uniqueness: true, presence: true, length: {maximum: 105}, format: { with: URI::MailTo::EMAIL_REGEXP } 
+	validates :username, uniqueness: { case_sensitive: false}, 
+				presence: true, 
+				length: { minimum: 3, maximum: 25 }
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	
+	validates :email, uniqueness: { case_sensitive: false}, 
+				presence: true, 
+				length: { maximum: 105 }, 
+				format: { with: VALID_EMAIL_REGEX } 
 
 end
