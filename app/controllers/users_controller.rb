d37f5cbf12b5ae3a8af07c827579a4d3315a16bd
Stefan L'Esperance <slesperance@conamex.com>
2021-04-15 07:17:09 -0400

    Added User Edit/Update Functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
    def create
-    @user = Users.new(user_params)
+    @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
@@ -14,7 +14,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
