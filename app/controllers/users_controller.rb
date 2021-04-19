326d5c8665276a97e30069b3a4ede2e626ca3647
Stefan L'Esperance <slesperance@conamex.com>
2021-04-19 11:22:58 -0400

    Test.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,10 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page], per_page: 5)
   end
 
-   def create
+  def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id 
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
