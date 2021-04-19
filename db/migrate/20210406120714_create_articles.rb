0d290505b2c431efb8ad873416b96fe19687fbc1
Stefan L'Esperance <slesperance@conamex.com>
2021-04-19 05:36:05 -0400

    DB Issue.

--- db/migrate/20210406120714_create_articles.rb
+++ db/migrate/20210406120714_create_articles.rb
@@ -1,11 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
-      #T.Type :Name
       t.string :title
       t.text :description
-
-      t.timestamps
     end
   end
 end
