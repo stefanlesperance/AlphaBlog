2b91a924b5a2ac3ff38cbe441a187c44d7b63f2f
Stefan L'Esperance <slesperance@conamex.com>
2021-04-20 07:26:27 -0400

    Category Creation - Test Driven Coding.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,34 @@
 class CategoriesController < ApplicationController
+
+	def new
+		@category = Category.new
+	end
+
 	def index
 	end
+
+	def create
+		@category = Category.new(category_params)
+		if @category.save
+			flash[:notice] = "Category successfully created"
+			redirect_to @category
+		else
+			render 'new'
+		end
+	end
+
 
 	def show
 	end
 
-	def new
+
+	private
+
+	def category_params
+		#Note, the :category - is the top level key. Need to know more on this
+		params.require(:category).permit(:name)
 	end
-	
+
+
 
 end
