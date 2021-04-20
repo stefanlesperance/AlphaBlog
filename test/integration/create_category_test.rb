d90602414e7c6f7f0c837eabbcd54c6835080eca
Stefan L'Esperance <slesperance@conamex.com>
2021-04-20 07:55:28 -0400

    Minor Adjustments.

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -13,4 +13,18 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "Sports", response.body
   end
+
+    test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference 'Category.count' do
+    	post categories_path, params: { category: { name: ""}}
+    end
+    #How to test for error messages
+    #You can test for the HTML element unique/specific to the error partial
+    #Also possible to test for specifics
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
 end
