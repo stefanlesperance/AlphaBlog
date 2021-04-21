985bbaf2ba9db5ae37ef5569deedc7c339738b6e
Stefan L'Esperance <slesperance@conamex.com>
2021-04-21 09:40:55 -0400

    Integration Tests Functional.

--- test/integration/article_creation_test.rb
+++ test/integration/article_creation_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class ArticleCreationTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+
+  setup do
+    @user = User.create(username: "johndoe", email: "johndoe@example.com", 
+                      password: "password")
+  	sign_in_as(@user)
+  	#If I wanted to be super sure about this, I'd try and delete it with the same user and see what happened.
+  end
+
+
+
+  test "get new article form and create article" do
+
+    get "/articles/new"
+    assert_response :success
+    assert_difference 'Article.count', 1 do
+    	post articles_path, params: { article: { title: "spectest", description: "this is a test"}}
+    	assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+  end
+
+
 end
--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "sign up new user" do
+    get "/signup"
+    assert_response :success
+    assert_difference 'User.count', 1 do
+    	post users_path, params: { user: { username: "spectest", email: "slesperance@conamex.com", password: "password4U"} }
+    	assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+  end
+
+
+
 end
